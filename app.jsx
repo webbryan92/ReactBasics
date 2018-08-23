c14a7e71022490f1f1b6b1c1d54103925823b572
Ryan Webb <webbryan92@gmail.com>
2018-08-23 16:31:40 -0400

    Creating a component Class

--- app.jsx
+++ app.jsx
@@ -46,19 +46,20 @@ Player.propTypes = {
     score: React.PropTypes.number.isRequired,
 }
 
-function Counter(props) {
-    return (
-        <div className="counter">
-            <button className="counter-action decrement"> - </button>
-            <div className="counter-score"> {props.score} </div>
-            <button className="counter-action increment"> + </button>
-        </div>
-    );
-}
-
-Counter.propTypes = {
-    score: React.PropTypes.number.isRequired,
-}
+var Counter = React.createClass({
+    propTypes: {
+        score: React.PropTypes.number.isRequired,
+    },
+    render: function() {
+        return (
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> {this.props.score} </div>
+                <button className="counter-action increment"> + </button>
+            </div>
+        );
+    }
+});
 
 function Application(props) {
     return (
