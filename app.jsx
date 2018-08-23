6ec3324f07cc5a92b281b84e4d92060ca4f5fef2
Ryan Webb <webbryan92@gmail.com>
2018-08-23 02:50:04 -0400

    refactoring

--- app.jsx
+++ app.jsx
@@ -1,53 +1,55 @@
-function Application (props){
+function Header(props) {
+    return (
+        <div className="header">
+            <h1>{props.title}</h1>
+        </div>
+    );
+}
+
+Header.propTypes = {
+    title: React.PropTypes.string.isRequired,
+};
+
+function Player(props) {
+    return (
+        <div className="player">
+            <div className="player-name">
+                {props.name}
+            </div>
+            <div className="player-score">
+                <Counter score={props.score} />
+            </div>
+        </div>
+    )
+}
+
+Player.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+}
+
+function Counter(props) {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <div className="counter-score"> {props.score} </div>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+Counter.propTypes = {
+    score: React.PropTypes.number.isRequired,
+}
+
+function Application(props) {
     return (
         <div className="scoreboard">
-            <div className="header">
-                <h1>{props.title}</h1>
-            </div>
-
+            <Header title={props.title} />
             <div className="players">
-                <div className="player">
-                    <div className="player-name">
-                        Bolt Vanderhuge
-                    </div>
-                    <div className="player-score">
-                        <div className="counter">
-                            <button className="counter-action decrement"> - </button>
-                            <div className ="counter-score"> 31 </div>
-                            <button className="counter-action increment"> + </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="players">
-                <div className="player">
-                    <div className="player-name">
-                        Vince McLarge
-                    </div>
-                    <div className="player-score">
-                        <div className="counter">
-                            <button className="counter-action decrement"> - </button>
-                            <div className ="counter-score"> 12 </div>
-                            <button className="counter-action increment"> + </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="players">
-                <div className="player">
-                    <div className="player-name">
-                        Wayne Wheeler
-                    </div>
-                    <div className="player-score">
-                        <div className="counter">
-                            <button className="counter-action decrement"> - </button>
-                            <div className ="counter-score"> 76 </div>
-                            <button className="counter-action increment"> + </button>
-                        </div>
-                    </div>
-                </div>
+                <Player name="Bolt Vanderhuge" score={31}></Player>
+                <Player name="Vince McLarge" score={12}></Player>
+                <Player name="Wayne Wheeler" score={76}></Player>
             </div>
         </div>
     );
@@ -61,4 +63,4 @@ Application.defaultProps = {
     title: "Scoreboard",
 }
 
-ReactDOM.render(<Application title = "My Scoreboard"/>, document.getElementById('container'));
+ReactDOM.render(<Application title="My Scoreboard" />, document.getElementById('container'));
