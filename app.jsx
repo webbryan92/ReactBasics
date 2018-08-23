bf9238ca772203448ae964c0b5989550193a8648
Ryan Webb <webbryan92@gmail.com>
2018-08-23 02:18:28 -0400

    properties

--- app.jsx
+++ app.jsx
@@ -1,14 +1,14 @@
-function Application (){
+function Application (props){
     return (
-        <div className="application">
+        <div className="scoreboard">
             <div className="header">
-                <h1>Scoreboard</h1>
+                <h1>{props.title}</h1>
             </div>
 
             <div className="players">
                 <div className="player">
                     <div className="player-name">
-                        Jim Hoskins
+                        Bolt Vanderhuge
                     </div>
                     <div className="player-score">
                         <div className="counter">
@@ -19,8 +19,38 @@ function Application (){
                     </div>
                 </div>
             </div>
+
+            <div className="players">
+                <div className="player">
+                    <div className="player-name">
+                        Vince McLarge
+                    </div>
+                    <div className="player-score">
+                        <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className ="counter-score"> 12 </div>
+                            <button className="counter-action increment"> + </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="players">
+                <div className="player">
+                    <div className="player-name">
+                        Wayne Wheeler
+                    </div>
+                    <div className="player-score">
+                        <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className ="counter-score"> 76 </div>
+                            <button className="counter-action increment"> + </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
 
-ReactDOM.render(<Application />, document.getElementById('container'));
+ReactDOM.render(<Application title = "My Scoreboard"/>, document.getElementById('container'));
