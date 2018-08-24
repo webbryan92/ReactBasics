3f494e8a0b333bc3937fe22820ac1bdbbba2d228
Ryan Webb <webbryan92@gmail.com>
2018-08-24 17:41:44 -0400

    updating state with new factoring

--- app.jsx
+++ app.jsx
@@ -31,15 +31,16 @@ Header.propTypes = {
 function Counter(props) {
     return (
         <div className="counter">
-            <button className="counter-action decrement" > - </button>
+            <button className="counter-action decrement" onClick={function() {props.onChange(-1);}}> - </button>
             <div className="counter-score"> {props.score} </div>
-            <button className="counter-action increment" > + </button>
+            <button className="counter-action increment" onClick={function() {props.onChange(1);}}> + </button>
         </div>
     );
 }
 
 Counter.propTypes = {
-    score: React.PropTypes.number.isRequired
+    score: React.PropTypes.number.isRequired,
+    onChange: React.PropTypes.func.isRequired,
 };
 
 function Player(props) {
@@ -49,7 +50,7 @@ function Player(props) {
                 {props.name}
             </div>
             <div className="player-score">
-                <Counter score={props.score}/>
+                <Counter score={props.score} onChange={props.onScoreChange}/>
             </div>
         </div>
     )
@@ -58,6 +59,7 @@ function Player(props) {
 Player.propTypes = {
     name: React.PropTypes.string.isRequired,
     score: React.PropTypes.number.isRequired,
+    onScoreChange: React.PropTypes.func.isRequired,
 };
 
 var Application = React.createClass({
@@ -82,17 +84,31 @@ var Application = React.createClass({
         };
     },
 
+    onScoreChange: function(index, delta){
+        console.log('onScoreChange', index, delta);
+        if(this.state.players[index].score !== 0 || delta === 1){
+            this.state.players[index].score += delta;
+            this.setState(this)
+        }
+    },
+
     render: function(){ 
         return (
             <div className="scoreboard">
                 <Header title={this.props.title} />
                 <div className="players">
-                    {this.state.players.map(function (player) {
-                        return <Player name={player.name} score={player.score} key={player.id} />
-                    })}
+                    {this.state.players.map(function (player, index) {
+                        return (
+                            <Player 
+                            onScoreChange={function(delta) {this.onScoreChange(index, delta)}.bind(this)}
+                            name={player.name} 
+                            score={player.score} 
+                            key={player.id} />
+                        )
+                    }.bind(this))} 
                 </div>
             </div>
-        );
+        ); /*when using ananymous functions you must call .bind(this) or youll lose the reference to this.*/
     }
 });
 
