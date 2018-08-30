96653366dca8babc3468899650f32a8c49f35845
Ryan Webb <webbryan92@gmail.com>
2018-08-29 22:08:06 -0400

    Add/Remove player functions

--- app.jsx
+++ app.jsx
@@ -15,6 +15,42 @@ var PLAYERS = [
         id: 3
     }
 ];
+var nextId = 4;
+
+var AddPlayerForm = React.createClass({
+    propTypes: {
+        onAdd: React.PropTypes.func.isRequired,
+    },
+
+    getInitialState: function() {
+        return {
+            name: "",
+        };
+    },
+
+    onNameChange: function(e){
+        console.log('onNameChange', e.target.value);
+        this.setState({name: e.target.value});
+    },
+
+    onSubmit: function(e){
+        e.preventDefault();
+
+        this.props.onAdd(this.state.name);
+        this.setState({name: ""});
+    },
+
+    render: function () {
+        return (
+            <div className="add-player-form">
+                <form onSubmit={this.onSubmit}>
+                    <input type="text" value ={this.state.name} onChange={this.onNameChange} />
+                    <input type="submit" value="Add Player" />
+                </form>
+            </div>
+        )
+    }
+});
 
 function Stats(props) {
     var totalPlayers = props.players.length;
@@ -74,6 +110,7 @@ function Player(props) {
     return (
         <div className="player">
             <div className="player-name">
+                <a className="remove-player" onClick={props.onRemove}>✖</a>
                 {props.name}
             </div>
             <div className="player-score">
@@ -87,6 +124,7 @@ Player.propTypes = {
     name: React.PropTypes.string.isRequired,
     score: React.PropTypes.number.isRequired,
     onScoreChange: React.PropTypes.func.isRequired,
+    onRemove: React.PropTypes.func.isRequired,
 };
 
 var Application = React.createClass({
@@ -117,6 +155,23 @@ var Application = React.createClass({
             this.state.players[index].score += delta;
             this.setState(this)
         }
+    },    
+
+    onPlayerAdd: function(name) {
+        console.log('Player added: ', name);
+        this.state.players.push({
+            name: name,
+            score: 0,
+            id: nextId,
+        });
+        this.setState(this.state);
+        nextId += 1;
+    },
+
+    onRemovePlayer: function(index) {
+        this.state.players.splice(index, 1);
+        this.setState(this.state);
+        console.log('remove', index);
     },
 
     render: function(){ 
@@ -128,13 +183,15 @@ var Application = React.createClass({
                         return (
                             <Player 
                             onScoreChange={function(delta) {this.onScoreChange(index, delta)}.bind(this)}
+                            onRemove={function() {this.onRemovePlayer(index)}.bind(this)}
                             name={player.name} 
                             score={player.score} 
                             key={player.id} />
                         )
                     }.bind(this))} 
                 </div>
-            </div>
+                <AddPlayerForm onAdd={this.onPlayerAdd} />
+            </div>            
         ); /*when using ananymous functions you must call .bind(this) or youll lose the reference to this.*/
     }
 });
